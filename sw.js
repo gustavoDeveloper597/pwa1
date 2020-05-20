ae936389e76a8ad4ea4c03a092ccc0fbdcc563e5
Gustavo <ab1d@hotmail.es>
2020-05-20 15:45:58 -0500

    correcciones para ios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utilis.js');
 //https://gustavodeveloper597.github.io/pwa1/
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DINAMIC_CACHE = 'dinamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
